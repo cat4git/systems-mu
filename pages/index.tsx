969821d776c26e5c9eb1cf13b18223be9a70f6e5
cat4git <cat4mail@gmail.com>
2020-07-06 09:41:54 +0300

    fix sum bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,19 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../components/date';
-import { GetStaticProps } from 'next';
-import fetch from 'isomorphic-unfetch';
-import data from '../mock/systems.json';
 import List from '@material-ui/core/List';
-import { listStyles } from './list.style'
-import ListSubheader from '@material-ui/core/ListSubheader'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../components/layout';
 import { getSystemInfo } from '../lib/system';
+import utilStyles from '../styles/utils.module.css';
+
+
+
 
 export default function Home({
   allPostsData}) {
-  const classes = listStyles();
+  
 
   return (
     <Layout home>
@@ -39,10 +36,10 @@ export default function Home({
             </li>
           ))}
         </ul> */}
-        <List className={classes.root} subheader={<li />}>
+        <List  subheader={<li />}>
         {allPostsData.map((parent) => (
-          <li key={`item-${parent}`} className={classes.listSection}>
-            <ul className={classes.ul}>
+          <li key={`item-${parent}`} >
+            <ul>
             <ListItemText  >
               <Link href="/posts/[id]" as={`/posts/${parent.type.name.toLowerCase().replace(" ","-")}`}>
                 <a >{`${parent.name} (${parent.type.name}) ${parent.status}`}</a>
